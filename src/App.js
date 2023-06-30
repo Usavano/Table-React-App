07a9b36a3f3a069e3cdce7853ac4e2eb1061bb88
Ivan Usatyi <usaivan2003@gmail.com>
2023-06-30 15:54:03 +0300

    Adding cross-data search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import Table from './components/Table';
-import { Pagination } from '@mui/material';
 
 function App() {
   const [carsData, setCarsData] = useState([]);
@@ -17,9 +16,6 @@ function App() {
 
   const lastRowIndex = currentPage * recordsPerPage;
   const firstRowIndex = lastRowIndex - recordsPerPage;
-  //   const records = carsData.slice(firstRowIndex, lastRowIndex);
-
-  const numberOfPages = Math.ceil(carsData.length / recordsPerPage);
 
   const handlePages = (e, p) => {
     setCurrentPage(p);
@@ -32,21 +28,6 @@ function App() {
     }
   };
 
-  //   This normal functions for pagination
-  //   const nextPage = () => {
-  //     if (currentPage <= numberOfPages - 1) {
-  //       setCurrentPage(currentPage + 1);
-  //     }
-  //   };
-  //   const prevPage = () => {
-  //     if (currentPage >= 2) {
-  //       setCurrentPage(currentPage - 1);
-  //     }
-  //   };
-  //   const changeCurrPage = (id) => {
-  //     setCurrentPage(id);
-  //   };
-
   return (
     <div className='app'>
       <input
@@ -54,19 +35,15 @@ function App() {
         placeholder='Search..'
         autoComplete='false'
         onInput={handleSearchValue}
+        className='searchBar'
       />
       <Table
         records={carsData}
         srchVal={searchTerm}
         firstRowIndex={firstRowIndex}
         lastRowIndex={lastRowIndex}
-      />
-      <Pagination
-        count={numberOfPages}
-        onChange={handlePages}
-        size='large'
-        variant='outlined'
-        shape='rounded'
+        recordsPerPage={recordsPerPage}
+        handlePages={handlePages}
       />
     </div>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,38 +1,56 @@
 import Row from './Row';
 import './Table.css';
+import { Pagination } from '@mui/material';
 
-function Table({ records, srchVal, firstRowIndex, lastRowIndex }) {
+function Table({
+  records,
+  srchVal,
+  firstRowIndex,
+  lastRowIndex,
+  recordsPerPage,
+  handlePages,
+}) {
+  const filteredData = records.filter((obj) => {
+    return Object.values(obj).some((val) => {
+      return val.toString().toLowerCase().includes(srchVal.toLowerCase());
+    });
+  });
+
+  const numberOfPages = Math.ceil(filteredData.length / recordsPerPage);
+
   return (
-    <table className='table'>
-      <thead className='table__head theader'>
-        <tr className='theader__row'>
-          <th className='theader__item'>Company</th>
-          <th className='theader__item'>Model</th>
-          <th className='theader__item'>Vin</th>
-          <th className='theader__item'>Color</th>
-          <th className='theader__item'>Year</th>
-          <th className='theader__item'>Price</th>
-          <th className='theader__item'>Availability</th>
-          <th className='theader__item'>Actions columns</th>
-        </tr>
-      </thead>
+    <div className='table-container'>
+      <table className='table'>
+        <thead className='table__head theader'>
+          <tr className='theader__row'>
+            <th className='theader__item'>Company</th>
+            <th className='theader__item'>Model</th>
+            <th className='theader__item'>Vin</th>
+            <th className='theader__item'>Color</th>
+            <th className='theader__item'>Year</th>
+            <th className='theader__item'>Price</th>
+            <th className='theader__item'>Availability</th>
+            <th className='theader__item'>Actions columns</th>
+          </tr>
+        </thead>
 
-      <tbody className='table_body tbody'>
-        {records
-          .filter((obj) => {
-            return Object.values(obj).some((val) => {
-              return val
-                .toString()
-                .toLowerCase()
-                .includes(srchVal.toLowerCase());
-            });
-          })
-          .map((car) => {
-            return <Row {...car} key={car.id} />;
-          })
-          .slice(firstRowIndex, lastRowIndex)}
-      </tbody>
-    </table>
+        <tbody className='table_body tbody'>
+          {filteredData
+            .map((car) => {
+              return <Row {...car} key={car.id} />;
+            })
+            .slice(firstRowIndex, lastRowIndex)}
+        </tbody>
+      </table>
+      <Pagination
+        count={numberOfPages}
+        onChange={handlePages}
+        size='large'
+        variant='outlined'
+        shape='rounded'
+        className='table-container__pagination'
+      />
+    </div>
   );
 }
 
